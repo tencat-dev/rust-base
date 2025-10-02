7c15ecaf064fa0490051520f8dcddc7dc8050986
anhnmt <anhng.dev@gmail.com>
2025-10-02 22:45:35 +0700

    Add devenv

---
+++ devenv.nix
@@ -0,0 +1,53 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.rust = {
+      channel = "stable";
+      enable = true;
+      version = "1.90.0";
+      targets = ["x86_64-unknown-linux-gnu"];
+    };
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+#  scripts.hello.exec = ''
+#    echo hello from $GREET
+#  '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    git --version # Use packages
+    echo "Rust version: $(rustc --version)"
+    echo "Cargo version: $(cargo --version)"
+    echo "RUST_SRC_PATH: $RUST_SRC_PATH"
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
